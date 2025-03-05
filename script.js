25c17b4f11473748a47a9c2191603a8d80a20448
DeepanshuShuklaOfficial <deepanshushukla222@gmail.com>
2025-03-05 21:19:25 +0530

    card section

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ const slideImage = () => {
 
 // Auto-slide every 5 seconds
 setInterval(goNext, 5000);
+
+
+// for card section 
+
+let index = 0;
+const cardWrapper = document.getElementById("cardWrapper");
+const totalCards = document.querySelectorAll(".card").length;
+
+function getCardsToShow() {
+    return window.innerWidth < 768 ? 2 : 4; // true pe mobile view , false pe pc view
+}
+
+function updateSlide() {
+    const cardsToShow = getCardsToShow();
+    const totalSlides = Math.ceil(totalCards / cardsToShow);
+
+    index = Math.max(0, Math.min(index, totalSlides - 1));
+    const translateX = index * -100;
+    cardWrapper.style.transform = `translateX(${translateX}%)`;
+}
+
+function nextSlide() {
+    index++;
+    updateSlide();
+}
+
+function prevSlide() {
+    index--;
+    updateSlide();
+}
+
+window.addEventListener("resize", updateSlide);
