3cf0df4e6c62ebf8e46010f477a483978c571c16
DeepanshuShuklaOfficial <deepanshushukla222@gmail.com>
2025-03-12 18:03:38 +0530

    hamburger of menu

--- script.js
+++ script.js
@@ -131,3 +131,18 @@ function scrollToTop() {
         header.scrollIntoView({ behavior: "smooth" });
     }
 }
+
+
+// for hamburger menu
+
+const menuBtn = document.getElementById('menuBtn');
+const closeBtn = document.getElementById('closeBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('translate-x-full'); // Slide in
+});
+
+closeBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('translate-x-full'); // Slide out
+});
