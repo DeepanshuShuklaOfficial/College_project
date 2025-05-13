3da621e9fe459c26d95039aefb7645dab454f5af
DeepanshuShuklaOfficial <deepanshushukla222@gmail.com>
2025-05-13 12:12:09 +0530

    ok

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD:javascript/index.js
-=======
-// for best offer and top destination card generation
-const cards = [
-    { name: "Ayodhya", location: "Uttar Pradesh", img: "images/card/Ayodhya, Uttar Pradesh/1.jpg", price: "₹8,000", discount: "Save 18%" },
-    { name: "Taj Mahal", location: "Agra", img: "images/card/Taj Mahal, Agra/1.jpg", price: "₹6,000", discount: "Save 25%" },
-    { name: "Kashi Vishwanath", location: "Varanasi", img: "images/card/Kashi Vishwanath Temple, Varanasi/1.jpg", price: "₹10,000", discount: "Save 12%" },
-    { name: "Shirdi Sai Baba", location: "Maharashtra", img: "images/card/Shirdi Sai Baba Temple, Maharashtra/1.jpg", price: "₹5,500", discount: "Save 30%" },
-    { name: "Jaipur", location: "Rajasthan", img: "images/card/Jaipur, Rajasthan/1.jpg", price: "₹9,000", discount: "Save 20%" },
-    { name: "Mumbai", location: "Maharashtra", img: "images/card/Mumbai, Maharashtra/1.jpg", price: "₹12,000", discount: "Save 15%" },
-    { name: "Udaipur", location: "Rajasthan", img: "images/card/Udaipur, Rajasthan/1.jpg", price: "₹10,000", discount: "Save 28%" },
-    { name: "Rishikesh", location: "Uttarakhand", img: "images/card/Rishikesh, Uttarakhand/1.jpg", price: "₹8,500", discount: "Save 22%" }
-];
-
-// Top Destination Data
-const topDestinations = [
-    { name: "Goa", img: "images/card/Goa/1.jpg" },
-    { name: "Kerala Backwaters", img: "images/card/Kerala Backwaters/1.jpg" },
-    { name: "Delhi", img: "images/card/Delhi/1.jpg" },
-    { name: "Mysore, Karnataka", img: "images/card/Mysore, Karnataka/1.jpg" },
-    { name: "Hampi, Karnataka", img: "images/card/Hampi, Karnataka/1.jpg" },
-    { name: "Jaisalmer, Rajasthan", img: "images/card/Jaisalmer, Rajasthan/1.jpg" },
-    { name: "Amritsar, Punjab", img: "images/card/Amritsar, Punjab/1.jpg" },
-    { name: "Shimla, Himachal Pradesh", img: "images/card/Shimla, Himachal Pradesh/1.jpg" },
-    { name: "Manali, Himachal Pradesh", img: "images/card/Manali, Himachal Pradesh/1.jpg" },
-    { name: "Darjeeling, West Bengal", img: "images/card/Darjeeling, West Bengal/1.jpg" },
-    { name: "Leh-Ladakh, Jammu and Kashmir", img: "images/card/Leh-Ladakh, Jammu & Kashmir/1.jpg" },
-    { name: "Khajuraho, Madhya Pradesh", img: "images/card/Khajuraho, Madhya Pradesh/1.jpg" },
-    { name: "Ajanta and Ellora Caves, Maharashtra", img: "images/card/Ajanta and Ellora Caves, Maharashtra/1.webp" },
-    { name: "Sundarbans, West Bengal", img: "images/card/Sundarbans, West Bengal/1.jpg" },
-    { name: "Andaman and Nicobar Islands", img: "images/card/Andaman and Nicobar Islands/1.jpg" }
-];
-
->>>>>>> e928c72448b4079e945abdf8ca7f3934cf84cc91:index.js
 // Generate Cards
 function generateCards() {
     const cardWrapper = document.getElementById("cardWrapper");
